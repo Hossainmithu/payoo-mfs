437d262dddfe5746b37b3f774370438703c7c88a
hossainmithu <hossainmithu248@gmail.com>
2024-09-19 09:07:13 +0600

    added home page login btn

---
+++ js/login.js
@@ -0,0 +1,8 @@
+// console.log('I love youuuuuuu');
+document.getElementById('login-btn').addEventListener('click', function(event){
+  event.preventDefault();
+  console.log('I love YOUUUUUUUUUUUUUUUU');
+
+  const phoneNumber = document.getElementById('phone-number').value;
+  console.log(phoneNumber);
+})
